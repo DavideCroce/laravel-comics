ef28457b092c835d11512d00402cdc272b0db7a9
IskahnX <davidecroce10@gmail.com>
2022-03-16 17:20:00 +0100

    added top footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>DC Comics</title>
 </head>
 
@@ -35,6 +36,7 @@
             </ul>
             <div class="searchbar">
                 <input type="text" placeholder="SEARCH">
+                <i class="fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
 
@@ -124,64 +126,69 @@
     </main>
     <footer>
         <div class="top-footer">
-            <div class="dc-comics-shop">
-                <h1>DC COMICS</h1>
-                <ul>
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>TV</li>
-                    <li>Games</li>
-                    <li>Videos</li>
-                    <li>News</li>
-                </ul>
-                <h1>SHOP</h1>
-                <ul>
-                    <li>Shop DC</li>
-                    <li>Shop DC Collectibles</li>
-                </ul>
+            <div class="container">
+                <div class="footer-links">
+                    <div class="lists">
+                        <div class="dc-comics-shop">
+                            <h1>DC COMICS</h1>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                            <h1>SHOP</h1>
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </div>
+                        <div class="dc">
+                            <h1>DC</h1>
+                            <ul>
+                                <li>Terms Of Use</li>
+                                <li>Privacy Policy (new)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscription</li>
+                                <li>Talent Workshop</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </div>
+                        <div class="sites">
+                            <h1>SITES</h1>
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="cookies-settings">
+                        <p>All Sites Content TM and c 2020 DC Enterteinment, unless otherwise <a href="#">noted here</a>. All rights reserved. <a href="#">Cookies Settings</a></p>
+                    </div>
+                </div>
+                <div class="dc-bg"></div>
+                <!-- <img src="{{ asset('images/dc-logo-bg.png') }}" alt=""> -->
             </div>
-            <div class="dc">
-                <h1>DC</h1>
-                <ul>
-                    <li>Terms Of Use</li>
-                    <li>Privacy Policy (new)</li>
-                    <li>Ad Choices</li>
-                    <li>Advertising</li>
-                    <li>Jobs</li>
-                    <li>Subscription</li>
-                    <li>Talent Workshop</li>
-                    <li>CPSC Certificates</li>
-                    <li>Ratings</li>
-                    <li>Shop Help</li>
-                    <li>Contact Us</li>
-                </ul>
-            </div>
-            <div class="sites">
-                <h1>SITES</h1>
-                <ul>
-                    <li>DC</li>
-                    <li>MAD Magazine</li>
-                    <li>DC Kids</li>
-                    <li>DC Universe</li>
-                    <li>DC Power Visa</li>
-                </ul>
-            </div>
-            <div class="cookies-settings">
-                <p>All Sites Content TM and c 2020 DC Enterteinment, unless otherwise <a href="#">noted here</a>. All rights reserved. <a href="#"></a>Cookies Settings</p>
-            </div>
-            <img src="{{ asset('images/dc-logo-bg.png') }}" alt="">
         </div>
-        <div class="bottom-header">
+        <div class="bottom-footer">
             <button>SIGN-UP NOW!</button>
             <h4>FOLLOW US</h4>
-            <ul>
-                <li>FACEBOOK</li>
-                <li>TWITTER</li>
-                <li>YOUTUBE</li>
-                <li>PINTEREST</li>
-                <li>GEOGUESSER</li>
-            </ul>
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-youtube"></i>
+            <i class="fa-brands fa-pinterest-p"></i>
+            <i class="fa-solid fa-location-dot"></i>
         </div>
     </footer>
 </body>
