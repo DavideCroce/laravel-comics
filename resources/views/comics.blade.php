b4d901329086b852f6dd47f1503fb70846cdd079
IskahnX <davidecroce10@gmail.com>
2022-03-17 19:02:28 +0100

    added single comic section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -2,18 +2,18 @@
 
 
 @section('content')
-
-<div class="current-series">
-    <div class="card-section">
-        @foreach($comics as $comic)
-        <div class="card">
-            <img src="{{ $comic['thumb'] }}" alt="">
-            <p>{{ $comic['series'] }}</p>
+    <div class="current-series">
+        <div class="card-section">
+            @foreach ($comics as $comic)
+                <a href="">
+                    <div class="card">
+                        <div class="thumb" style="background-image: url({{ $comic['thumb'] }})"></div>
+                        <p>{{ $comic['series'] }}</p>
+                    </div>
+                </a>
+            @endforeach
         </div>
-        @endforeach
+        <!-- <div>CURRENT SERIES</div> -->
+        <button>LOAD MORE</button>
     </div>
-    <!-- <div>CURRENT SERIES</div> -->
-    <button>LOAD MORE</button>
-</div>
-
 @endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="dc-family">
+        <h1>WELCOME TO THE DC FAMILY!!</h1>
+    </div>
+@endsection
